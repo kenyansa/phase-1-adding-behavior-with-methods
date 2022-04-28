abe148f772e37aa2328358a0504f74e2574983d5
Kenyansa <kenyansajustine@gmail.com>
2022-04-28 07:50:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+//  cat: "before each" hook for "has a class of cat":
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    return `${this.name} says meow!`;
+  }
+}
+// dog:"before each" hook for "has a class of dog":
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    return `${this.name} says woof!`;
+  }
+}
+// bird: "before each" hook for "has a class of Bird":
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    if (this.sex === "male") {
+      return `It's me! ${this.name}, the parrot!`;
+    } else {
+      return `${this.name} says squawk!`;
+    }
+  }
+}
